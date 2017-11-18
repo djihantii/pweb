4411af33276f2528eebfc43a6d1de3df3c385973
Quentin YE <yijian258@gmail.com>
2017-11-18 01:42:49 +0100

    Ajout index

--- php/Connexion.class.php
+++ php/Connexion.class.php
@@ -1,8 +1,10 @@
 <?php
 class Connexion {
 	const DNS = 'mysql:host=localhost;dbname=pweb';
-	const USER = 'marvin';
-	const PWD = 'marvin';
+	//const USER = 'marvin';
+	//const PWD = 'marvin';
+	const USER = 'root';
+	const PWD = 'root';
 	const CHARSET = 'utf8';
 }
 ?>
